6dc0815f3d41e790f01bf88069cfe1cc4f28cc86
hexofiteration <hexofiteration@gmail.com>
2026-05-25 12:29:49 +0200

    modify sumAll()
    to take 2 positive integers and
    return the sum of every integer between (and including) them

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  
+  if( start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end) ) return "ERROR";
+  
+  let arr = [];
+  let result = 0;
+  
+  if(start > end){
+    
+    for (var i = end; i <= start; i++) {
+      result += i;
+    }
+  } else {
+    
+    for (var i = start; i <= end; i++) {
+      result += i;
+    }
+  }
+    
+  return result;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
