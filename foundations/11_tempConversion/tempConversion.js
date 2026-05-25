1c4fc8104437b17bb5de0ff2e11d9d1e672857ac
hexofiteration <hexofiteration@gmail.com>
2026-05-25 14:06:04 +0200

    modify convertToCelsius and convertToFahrenheit
    to convert temperatures from Fahrenheit to Celsius, and vice versa

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let cel = (x - 32) * (5/9);
+
+  if(!Number.isInteger(cel)) return parseFloat(cel.toFixed(1));
+  return cel;
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(x) {
+  let fah = x * (9/5) +32;
 
+  if(!Number.isInteger(fah)) return parseFloat(fah.toFixed(1));
+  return fah;
+};
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
