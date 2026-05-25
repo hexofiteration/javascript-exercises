496fa51ef755cc004b52959458dd1b424a9c41b0
hexofiteration <hexofiteration@gmail.com>
2026-05-25 11:55:57 +0200

    modify removeFromArray()
    to take an array and some other arguments then remove the other
    arguments from that array, and return the resulting array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArr, ...inputN) {
+  return inputArr.filter(item => !inputN.some(n => n === item));
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
