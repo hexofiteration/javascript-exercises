8f5802f1d9bdd1d8544e30bf3610adaca7f4fd2d
hexofiteration <hexofiteration@gmail.com>
2026-06-14 09:41:59 +0200

    modify sum()
    to take an array of numbers and add all the numbers together

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -6,8 +6,8 @@ const subtract = function(num1, num2) {
 	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, el) => acc += el, 0);
 };
 
 const multiply = function() {
