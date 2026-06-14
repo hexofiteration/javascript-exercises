a11803d1f75f21984c628254882cc70479917de7
hexofiteration <hexofiteration@gmail.com>
2026-06-14 11:54:14 +0200

    modify fibonacci()
    to return a specific member of the Fibonacci sequence

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(Number(n) < 0) return "OOPS";
+    if(Number(n) <= 1) return Number(n);
+    
+    let prev = 0;
+    let curr = 1;
+    
+    for (let i = 2; i <= n; i++) {
+        const next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
 };
 
 // Do not edit below this line
