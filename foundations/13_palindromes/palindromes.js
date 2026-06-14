e95c131f4476a30da9eb3597147bbcb88595848d
hexofiteration <hexofiteration@gmail.com>
2026-06-14 10:42:29 +0200

    modify palindromes()
    to determine whether or not a given string is a palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newStr = str.toLowerCase().replaceAll(/[^a-zA-Z0-9]/g,"")
+    let reverseStr = newStr.split("").reverse().join("");
 
+    return (newStr === reverseStr)
 };
 
 // Do not edit below this line
