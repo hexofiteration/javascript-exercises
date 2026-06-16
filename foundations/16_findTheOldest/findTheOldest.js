98592de417f216bc0aecce36be5153e5bbfc9fc0
hexofiteration <hexofiteration@gmail.com>
2026-06-16 14:14:25 +0200

    modify findTheOldest()
    to return the oldest person based on an array of objects representing
    people with a birth and death year

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = (new Date()).getFullYear();
 
+    const oldest = array.reduce((acc, curr) => {
+        const age = Object.hasOwn(curr, 'yearOfDeath')
+            ? curr.yearOfDeath - curr.yearOfBirth
+            : currentYear - curr.yearOfBirth;
+
+        if (age > acc.age) {
+            return { name: curr.name, age };
+        }
+        return acc;
+    }, { name: null, age: 0 });
+
+    return oldest;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
